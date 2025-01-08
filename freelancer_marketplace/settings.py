8e3584e4d840bbaf826a994554a4ff372d284383
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2025-01-08 14:00:35 +0600

    change minor things

--- freelancer_marketplace/settings.py
+++ freelancer_marketplace/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s^#&5)7-3362bgwrpplnk_6=0*s%=j1f!vqvo378q1($0%7+dy'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
